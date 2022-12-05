4972cd8efcc0d0ca8aa83477a3efd09c9d91d3eb
mimofun <newlight12210@gmail.com>
2022-12-05 12:44:32 +0600

    batge start

--- app/src/main/java/com/example/finalmimofun/Varifi_otp_Activity.java
+++ app/src/main/java/com/example/finalmimofun/Varifi_otp_Activity.java
@@ -16,11 +16,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
 
 public class Varifi_otp_Activity extends AppCompatActivity {
 
@@ -30,6 +34,9 @@ public class Varifi_otp_Activity extends AppCompatActivity {
     private ProgressBar progressBarVerify;
 
     private String verificationId;
+
+    private FirebaseAuth mAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +54,8 @@ public class Varifi_otp_Activity extends AppCompatActivity {
         btnVerify = findViewById(R.id.btnVerify);
         progressBarVerify = findViewById(R.id.progressBarVerify);
 
+        mAuth = FirebaseAuth.getInstance();
+
         editTextInput();
 
         tvMobile.setText(String.format(
@@ -58,7 +67,15 @@ public class Varifi_otp_Activity extends AppCompatActivity {
         tvResendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Varifi_otp_Activity.this, "OTP Send Successfully.", Toast.LENGTH_SHORT).show();
+                etC1.setText("");
+                etC2.setText("");
+                etC3.setText("");
+                etC4.setText("");
+                etC5.setText("");
+                etC6.setText("");
+                otpSend();
+
+
             }
         });
 
@@ -195,6 +212,46 @@ public class Varifi_otp_Activity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void otpSend() {
+        progressBarVerify.setVisibility(View.VISIBLE);
+        tvResendBtn.setVisibility(View.INVISIBLE);
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+                progressBarVerify.setVisibility(View.GONE);
+                tvResendBtn.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                progressBarVerify.setVisibility(View.GONE);
+                tvResendBtn.setVisibility(View.VISIBLE);
+                Toast.makeText(Varifi_otp_Activity.this, "OTP is successfully send.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Varifi_otp_Activity.this,Varifi_otp_Activity.class));
+
+            }
+        };
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+88" + tvMobile.getText().toString().trim())
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(mCallbacks)
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
 
